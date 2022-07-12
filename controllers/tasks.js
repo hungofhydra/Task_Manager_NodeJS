aace677e37baf7cd06e53c604d5688de2a2cfedb
Nguyen Tan Hung <hungofhydraforwork@gmail.com>
2022-07-12 09:28:18 +0700

    Finish Get All Tasks Api V1

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,8 +1,14 @@
 const Task = require('../models/tasks')
 
 const getAllTasks = async (req, res) =>{
-    res.send('All items from the file');
+    try {
+        const tasks = await Task.find().exec();
+        res.status(200).json({tasks});
+    } catch (error) {
+        res.status(500).json({'msg' : error});
+    }
 };
+
 
 const createTask = async (req,res) =>{
     try {
